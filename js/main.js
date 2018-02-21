7b6db6b44c33523db691427b79c44f327143d870
Ernestoaf <ernestoalonso953@gmail.com>
2018-02-21 13:28:08 +0100

    Añado flecha para subir, transiciones y scroll

--- js/main.js
+++ js/main.js
@@ -37,10 +37,41 @@ $(document).ready(function () {
     //Ventana modal
     $("#open").click(function(){
         $(".modal, .overlay").addClass("visible");
+        //para eliminar la clase transparente al hacer clic y evitar que el z-indez se superponga a los demas elementos
+        // $(".transparente").removeClass("transparente");
     });
 
     //Añadiendo la clase overlay conseguimos que se cierre tambien pulsando fuera de la ventana
     $("#close, .overlay").click(function(){
         $(".modal, .overlay").removeClass("visible");
+        // $(".transparente").addClass("transparente");
+    });
+
+    
+    // Scroll suave
+    $("a").on("click", function () { //on recibe uno o varios eventos
+        if (this.hash !== "") { // hash es lo que esta al lado de almohadilla
+            var hash = this.hash;
+            $("html, body").animate({
+                //objeto con propiedad y valor
+                scrollTop: $(hash).offset().top
+            }, 800);
+
+            //sintacis de animate
+            // animate({objeto}, duracion);
+        }
+    });
+
+    // Cambio de estilo segun scroll
+    $(window).on("scroll", function () {
+        var posicion = $("#primera").offset(); //almacenamos en la variable la poscion del elemto primera
+        console.log(posicion);
+        var barra = $(window).scrollTop();
+        console.log(barra);
+        if ($(window).scrollTop() > posicion.top - 50) { //scroll top coge la poscion de la ventana donde estas
+            $("header").removeClass("colornav");
+        } else {
+            $("header").addClass("colornav");
+        }
     });
 });
